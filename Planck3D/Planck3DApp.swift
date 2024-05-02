ef6ae29d4547b2ec1fcfb50016a4a1564e4d86e9
Maria Chemerys <129984077+MariaChemerys@users.noreply.github.com>
2024-05-02 12:42:10 +0200

    Initial Commit

---
+++ Planck3D/Planck3DApp.swift
@@ -0,0 +1,17 @@
+//
+//  Planck3DApp.swift
+//  Planck3D
+//
+//  Created by Mariia Chemerys on 02.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct Planck3DApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
