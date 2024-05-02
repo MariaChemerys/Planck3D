6d717f499fa7fd348d99c7ee29c982623d3401ca
Maria Chemerys <129984077+MariaChemerys@users.noreply.github.com>
2024-05-02 14:27:21 +0200

    Add the plot configuration to the viewDidLoad() function

--- Planck3D/UIViewControllerRepresentablePlanck3D.swift
+++ Planck3D/UIViewControllerRepresentablePlanck3D.swift
@@ -10,13 +10,7 @@ import Plot3d
 
 struct UIViewControllerRepresentablePlanck3D: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        PlanckDistributionUIViewControllerRepresentable()
     }
 }
 
@@ -32,7 +26,28 @@ struct PlanckDistributionUIViewControllerRepresentable: UIViewControllerRepresen
 class PlanckDistributionViewController: UIViewController{
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        // Configure the plot.
+        var config = PlotConfiguration()
+        config.xAxisHeight = 3
+        config.yAxisHeight = 4
+        config.zAxisHeight = 3
+        config.xTickInterval = 1
+        config.zTickInterval = 1
+        config.xMax = 6
+        config.zMax = 6
+
+        // Initialize the PlotView
+        let frame = CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height)
+        let plotView = PlotView(frame: frame, configuration: config)
+        view.addSubview(plotView)
+
+        // When using a custom configuration, the camera's position and orientation might need to be updated
+        plotView.setCamera(position: PlotPoint(10, 6, 10))
+        plotView.setCamera(lookAt: PlotPoint(0, 0, 0))
+
+        plotView.setAxisTitle(.x, text: "x axis", textColor: .white)
+        plotView.setAxisTitle(.y, text: "y axis", textColor: .white)
+        plotView.setAxisTitle(.z, text: "z axis", textColor: .white)
     }
 }
 
