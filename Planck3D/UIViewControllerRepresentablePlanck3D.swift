f483c37513824281e84cf951d20c5df143405ee1
Maria Chemerys <129984077+MariaChemerys@users.noreply.github.com>
2024-05-02 15:21:02 +0200

    Add the extensions that conform to the PlotDataSource and PlotDelegate protocols for the PlanckDistributionViewController class

--- Planck3D/UIViewControllerRepresentablePlanck3D.swift
+++ Planck3D/UIViewControllerRepresentablePlanck3D.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 import Plot3d
+import SceneKit
 
 struct UIViewControllerRepresentablePlanck3D: View {
     var body: some View {
@@ -48,6 +49,51 @@ class PlanckDistributionViewController: UIViewController{
         plotView.setAxisTitle(.x, text: "x axis", textColor: .white)
         plotView.setAxisTitle(.y, text: "y axis", textColor: .white)
         plotView.setAxisTitle(.z, text: "z axis", textColor: .white)
+        
+        plotView.dataSource = self
+        plotView.delegate = self
+        plotView.reloadData()
+    }
+}
+
+extension PlanckDistributionViewController: PlotDataSource{
+    func numberOfPoints() -> Int {
+            return 33 // The example in this artcile will only need a fixed number of points.
+        }
+}
+
+extension PlanckDistributionViewController: PlotDelegate{
+    // 1
+    func plot(_ plotView: PlotView, pointForItemAt index: Int) -> PlotPoint {
+        let v = CGFloat(index % 16)
+        if index < 16 {
+            return PlotPoint(cos(v) + 5, v, sin(v) + 5)
+        }
+        return PlotPoint(cos(v + 1.57) + 5, v, sin(v + 1.57) + 5)
+    }
+    
+    // 2
+    func plot(_ plotView: PlotView, geometryForItemAt index: Int) -> SCNGeometry? {
+        let geo = SCNSphere(radius: 0.15)
+        if index < 16  {
+            geo.materials.first!.diffuse.contents = UIColor.red
+        } else {
+            geo.materials.first!.diffuse.contents = UIColor.blue
+        }
+        return geo
+    }
+    
+    // 3
+    func plot(_ plotView: PlotView, textAtTickMark index: Int, forAxis axis: PlotAxis) -> PlotText? {
+        let config = PlotConfiguration()
+        switch axis {
+        case .x:
+            return PlotText(text: "\(index + 1)", fontSize: 0.3, offset: 0.25)
+        case .y:
+            return PlotText(text: "\(Int(CGFloat(index + 1) * config.yTickInterval))", fontSize: 0.3, offset: 0.1)
+        case .z:
+            return PlotText(text: "\(index + 1)", fontSize: 0.3, offset: 0.25)
+        }
     }
 }
 
